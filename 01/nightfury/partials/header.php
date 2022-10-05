1c3528d8ce3461c1de56b8d346cf68218a39f6b6
Marek <35346946+MarekKks@users.noreply.github.com>
2022-10-05 15:18:30 +0200

    Add files via upload

---
+++ 01/nightfury/partials/header.php
@@ -0,0 +1,87 @@
+<?php
+    $page_name = basename($_SERVER['SCRIPT_NAME']);
+    if ($page_name == 'index.php') $page_name = 'gallery.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Nightfury</title>
+
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&amp;subset=latin,latin-ext">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <!--[if lt IE 9]>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<header>
+    <div class="contact-bar">
+        <div class="container">
+            <ul class="menu personal">
+                <li><i class="fa fa-phone"></i><a href="tel:+421900111222">+421 900 111 222</a></li>
+                <li><i class="fa fa-envelope"></i><a href="mailto:email@mailinator.com">email@mailinator.com</a></li>
+            </ul>
+            <ul class="menu social">
+                <li><a href="#" class="social-icon"><i class="fa fa-github"></i></a></li>
+                <li><a href="#" class="social-icon"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#" class="social-icon"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="social-icon"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="nav-bar">
+        <div class="container">
+            <h1 class="logo">
+                <a href="#">
+                    <strong>NIGHT</strong>FURY<i class="fa fa-fire"></i>
+                </a>
+            </h1>
+
+            <nav class="group">
+                <ul class="menu navigation">
+                    <li><a href="index.php"><i class="fa fa-shield fa-2x"></i> Domov</a></li>
+                    <li class="selected"><a href="portfolio.php"><i class="fa fa-leaf fa-2x"></i> Portfolio</a></li>
+                    <li><a href="about.php"><i class="fa fa-bolt fa-2x"></i> O nás</a></li>
+                    <li><a href="contact.php"><i class="fa fa-trophy fa-2x"></i> Kontakt</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<main>
+		<article>
+			<header class="post-header">
+				<div class="container">
+					<h1 class="post-title">Portfólio našich prác</h1>
+				</div>
+			</header>
+			<div class="post-content">
+				<div class="container">
+
+					<h2 class="text-center">Toto je veta, ktorá je tu napísaná, ktorá má hovoriť o tom, ako veľmi sme šikovní sme v našom portfóliovaní a tvorbe vecí, ktoré tvoríme a taktiež vyrábame.</h2>
+
+					<ul class="menu controls">
+                        <?php
+                            $pages = (glob('*.php'));
+
+                            foreach ($pages as $file) {
+
+                                $page = $file;
+                                if ($page == 'index.php') $page = 'galery';
+
+                                if ($page_name == $page) echo '<li data-from="left"><a href="'.$page.'"><strong>'.basename($page, '.php').'</strong></a></li>';
+                                else echo '<li data-from="left"><a href="'.$file.'">'.basename($page, '.php').'</a></li>';
+                            }
+						?>
+					</ul>
+				</div>
---
+++ 01/nightfury/porfolio.php
@@ -0,0 +1,113 @@
+<?php
+include_once ('partials/header.php');
+?>
+<section class="gallery">
+					<div class="gallery-set container" id="vsetko">
+						<a href="img/samurai/image-1.jpg">
+							<img src="img/samurai/thumb-1.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="img/samurai/image-2.jpg">
+							<img src="img/samurai/thumb-2.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="img/samurai/image-3.jpg">
+							<img src="img/samurai/thumb-3.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="img/samurai/image-4.jpg">
+							<img src="img/samurai/thumb-4.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="img/samurai/image-5.jpg">
+							<img src="img/samurai/thumb-5.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="img/samurai/image-6.jpg">
+							<img src="img/samurai/thumb-6.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+                        <a href="img/zim/image-1.jpg">
+							<img src="img/zim/thumb-1.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="img/zim/image-2.jpg">
+							<img src="img/zim/thumb-2.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="img/zim/image-3.jpg">
+							<img src="img/zim/thumb-3.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="img/zim/image-4.jpg">
+							<img src="img/zim/thumb-4.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="img/zim/image-5.jpg">
+							<img src="img/zim/thumb-5.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="img/zim/image-6.jpg">
+							<img src="img/zim/thumb-6.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="img/zim/image-7.jpg">
+							<img src="img/zim/thumb-7.jpg" height="192" width="383" alt="space invader">
+						</a>
+                        <a href="img/morty/image-1.jpg">
+                            <img src="img/morty/thumb-1.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/morty/image-2.jpg">
+                            <img src="img/morty/thumb-2.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/morty/image-3.jpg">
+                            <img src="img/morty/thumb-3.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/morty/image-4.jpg">
+                            <img src="img/morty/thumb-4.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/morty/image-5.jpg">
+                            <img src="img/morty/thumb-5.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/morty/image-6.jpg">
+                            <img src="img/morty/thumb-6.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/morty/image-7.jpg">
+                            <img src="img/morty/thumb-7.jpg" height="192" width="383" alt="rick and morty">
+                        </a>
+                        <a href="img/adventure/image-1.jpg">
+                            <img src="img/adventure/thumb-1.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-2.jpg">
+                            <img src="img/adventure/thumb-2.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-3.jpg">
+                            <img src="img/adventure/thumb-3.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-4.jpg">
+                            <img src="img/adventure/thumb-4.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-5.jpg">
+                            <img src="img/adventure/thumb-5.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-6.jpg">
+                            <img src="img/adventure/thumb-6.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-7.jpg">
+                            <img src="img/adventure/thumb-7.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/adventure/image-8.jpg">
+                            <img src="img/adventure/thumb-8.jpg" height="192" width="383" alt="the dog the human">
+                        </a>
+                        <a href="img/dragon/image-1.jpg">
+                            <img src="img/dragon/thumb-1.jpg" height="192" width="383" alt="training dragons">
+                        </a>
+                        <a href="img/dragon/image-2.jpg">
+                            <img src="img/dragon/thumb-2.jpg" height="192" width="383" alt="training dragons">
+                        </a>
+                        <a href="img/dragon/image-3.jpg">
+                            <img src="img/dragon/thumb-3.jpg" height="192" width="383" alt="training dragons">
+                        </a>
+                        <a href="img/dragon/image-4.jpg">
+                            <img src="img/dragon/thumb-4.jpg" height="192" width="383" alt="training dragons">
+                        </a>
+                        <a href="img/dragon/image-5.jpg">
+                            <img src="img/dragon/thumb-5.jpg" height="192" width="383" alt="training dragons">
+                        </a>
+                        <a href="img/dragon/image-6.jpg">
+                            <img src="img/dragon/thumb-6.jpg" height="192" width="383" alt="training dragons">
+                        </a>
+					</div>
+</section>
+
+<?php
+include_once ('partials/footer.php');
+?>
